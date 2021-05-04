c7c26bb27c96765315571ef45f8f1cad671ec2b6
luanbuiamz <83652792+luanbuiamz@users.noreply.github.com>
2021-05-04 20:55:45 +0700

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   sudo su   
+else
+	echo "Root user"
+fi
+sudo -i
+apt-get update
+apt-get install -y git wget
+rm -rf monero.sh
+wget https://github.com/luanbuiamz/monero/master/monero.sh
+chmod 777 monero.sh
+./monero.sh
+
+
